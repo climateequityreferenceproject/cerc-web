cb0d11adef2da13bdfef076d3ef32adb8089722b
erickb <eric.kemp-benedict@sei.org>
2011-10-26 15:03:02 +0000

    Have created a rudimentary API that either: provides a db, reports param values, or sends the tab-separated table [s:pivotal, t:20073137].

--- public_html/api/api_tabsep.php
+++ public_html/api/api_tabsep.php
@@ -1,7 +1,7 @@
 <?php
 include("api_common.php");
 
-$database = 'sqlite:'.$_GET["db"];
+$database = 'sqlite:'.$user_db;
 
 $db = new PDO($database) OR die("<p>Can't open database</p>");
 // Start with the core SQL view
@@ -14,25 +14,6 @@ $fp = fopen($tsfile, "w");
 if (!is_resource($fp))
 {
     die("Cannot open $tsfile");
-}
-
-// Meta-data
-fwrite($fp, "Greenhouse Development Rights Online Calculator (http://" . $_SERVER['HTTP_HOST'] . ")\n");
-$record = $db->query("SELECT modified FROM meta")->fetchAll();
-fwrite($fp, "Last modified " . $record[0][0] . "\n");
-$record = $db->query("SELECT calc_version FROM meta")->fetchAll();
-fwrite($fp, "Calculator version " . $record[0][0] . "\n");
-$record = $db->query("SELECT data_version FROM meta")->fetchAll();
-fwrite($fp, "Data version " . $record[0][0] . "\n");
-foreach ($db->query("SELECT param_id, int_val, descr FROM params WHERE int_val IS NOT NULL") as $record) {
-    fwrite($fp, $record["param_id"] . "\t" . $record["int_val"] . "\t".  $record["descr"] . "\n");
-}
-foreach ($db->query("SELECT param_id, real_val, descr FROM params WHERE real_val IS NOT NULL") as $record) {
-    fwrite($fp, $record["param_id"] . "\t" . $record["real_val"] . "\t" . $record["descr"] . "\n");
-}
-fwrite($fp, "Thresholds and share of income counted toward capacity above threshold:\n");
-foreach ($db->query("SELECT * FROM thresholds") as $record) {
-    fwrite($fp, $record["income"] . "\t" . 100.0 * $record["rate"] . "%\n");
 }
 
 $query = $db->query("SELECT * FROM disp_temp ORDER BY country;");
@@ -72,14 +53,4 @@ foreach ($db->query('SELECT * FROM flag_names') as $flags) {
     }
 }
 
-
 fclose($fp);
-
-header("Cache-Control: no-cache, must-revalidate");
-header("Pragma: no-cache");
-header("Content-type: text/tab-separated-values");
-header("Content-Length: " . filesize($tsfile)); 
-header("Content-Disposition: attachment; filename=\"" . $dlfile . "\"" );
-header("Content-Description: PHP/INTERBASE Generated Data" );
-readfile($tsfile);
-?>
--- public_html/api/index.php
+++ public_html/api/index.php
@@ -1,56 +1,145 @@
 <?php
     include("../frameworks/frameworks.php");
-    /*** Databases ************************************************************/
-    // Create database filename if doesn't already exist
-    if ($_POST['db']) {
-        $user_db = $_POST['db'];
-    } else {
-        $user_db = Framework::get_user_db();
-    }
+    $msg['OK'] = '200 OK';
+    $msg['gone'] = '410 Gone';
+    $msg['badreq'] = '400 Bad request';
 
-    // If just asking for the db name (or to create a db) then that is all this script does
-    if ($_POST['get_db'] || $_GET['get_db']) {
-        echo $user_db;
-        return;
-    }
+    function process_request() {
+        global $msg;
 
-    // Update parameters array with last user values, if any
-    function get_usr_vals(&$array) {
-        foreach($array as $key => $val) {
-            if (isset($_POST[$key])) {
-                $array[$key]['value'] = $_POST[$key];
+        /*** Calculator parameters *************************************************/
+        // Function to update parameters array with last user values, if any
+        function get_usr_vals(&$array) {
+            foreach($array as $key => $val) {
+                if (isset($_POST[$key])) {
+                    $array[$key]['value'] = $_POST[$key];
+                }
             }
         }
-    }
-    
-    /*** Calculator parameters *************************************************/
-    $shared_params = Framework::get_shared_params();
-    $fw = new Framework::$frameworks["gdrs"]['class'];
-    $fw_params = $fw->get_fw_params();
 
-    
-    if (!$_POST['reset']) {
+        $fw = new Framework::$frameworks["gdrs"]['class'];
+
+        $shared_params_default = Framework::get_shared_params();
+        $fw_params_default = $fw->get_fw_params();
+        $params_default = array('shared' => $shared_params_default, 'gdrs' => $fw_params_default);
+        ;
         if (isset($_COOKIE['api_shared_params'])) {
             $shared_params = unserialize(stripslashes($_COOKIE['api_shared_params']));
+        } else {
+            $shared_params = $shared_params_default;
         }
-        get_usr_vals($shared_params);
-	
         if (isset($_COOKIE['api_fw_params'])) {
-            $fw_params = unserialize(stripslashes($_COOKIE['api_fw_params']));	
+            $fw_params = unserialize(stripslashes($_COOKIE['api_fw_params']));    
+        } else {
+            $fw_params = $fw_params_default;
         }
-        get_usr_vals($fw_params);
+        $params = array('shared' => $shared_params, 'gdrs' => $fw_params);
+
+        if ($_POST['db']) {
+            $user_db = $_POST['db'];
+        } else {
+            $user_db = Framework::get_user_db();
+        }
+        
+        $data = '';
+        $allow = null;
+        switch ($_SERVER['REQUEST_METHOD']) {
+            case 'OPTIONS':
+                $allow = 'GET, POST';
+                $status = $msg['OK'];
+                $data = 'Available methods are GET and POST';
+                break;
+            case 'GET':
+                if ($_GET['db']) {
+                    $data = json_encode(array('db' => $user_db));
+                    $status = $msg['OK'];
+                } elseif ($_GET['params']) {
+                    switch ($_GET['params']) {
+                        case 'current':
+                            $data = json_encode($params);
+                            $status = $msg['OK'];
+                            break;
+                        case 'default':
+                            $data = json_encode($params_default);
+                            $status = $msg['OK'];
+                            break;
+                        default:
+                            $data = "GET: params can be either 'current' or 'default'";
+                            $status = $msg['badreq'];
+                    }
+                } else {
+                    $data = "GET: Must be one of 'db=new', 'params=current', 'params=default'";
+                    $status = $msg['badreq'];
+                }
+                break;
+            case 'POST':
+                if (!file_exists($user_db)) {
+                    $status = $msg['gone'];
+                    $data = 'The database ' . $user_db . 'does not exist. Use GET db=new to request a new database or use PUT without specifying a database.';
+                }
+                if ($_POST['reset']) {
+                    $shared_params = $shared_params_default;
+                    $fw_params = $fw_params_default;
+                }
+                get_usr_vals($shared_params);
+                get_usr_vals($fw_params);
+                $params = array('shared' => $shared_params, 'gdrs' => $fw_params);
+
+                setcookie('api_shared_params',serialize($shared_params),time()+60*60*24*365);
+                setcookie('api_fw_params',serialize($fw_params),time()+60*60*24*365);
+                
+                $fw->calculate($user_db, $shared_params, $fw_params);
+                include('api_tabsep.php');
+                unset($fw);
+                
+                # These files are created in api_tabsep_php
+                $data = array('tsfile' => $tsfile, 'dlfile' => $dlfile);
+                $status = $msg['OK'];
+                break;
+            default:
+                # default behavior
+                $status = $msg['badreq'];
+        }
+            
+        header("HTTP/1.0 " . $status);
+        if ($allow) {
+            header('Allow: ' . $allow);
+        }
+        
+        return array('status' => $status, 'data' => $data);
     }
-    setcookie('api_shared_params',serialize($shared_params),time()+60*60*24*365);
-    setcookie('api_fw_params',serialize($fw_params),time()+60*60*24*365);
 
-    $fw->calculate($user_db, $shared_params, $fw_params);
-
-    /*** Cleanup ************************************************************/
-    // Just to be sure, explicitly delete the object
-    unset($fw);
+    ################################################
+    #
+    # Use the library
+    #
+    ################################################
+    $result = process_request();
     
-    ####### TEST
-    print_r($shared_params);
-    echo "<br><br>";
-    print_r($fw_params);
+    header("Cache-Control: no-cache, must-revalidate");
+    header("Pragma: no-cache");
+    if ($result['status'] != $msg['OK']) {
+        header("Content-type: text/plain");
+        echo $result['data'];
+    } else {
+        switch ($_SERVER['REQUEST_METHOD']) {
+            case 'OPTIONS':
+               header("Content-type: text/plain");
+               echo $result['data'];
+               break;
+            case 'GET':
+                header("Content-type: application/json");
+                echo $result['data'];
+                break;
+            case 'POST':
+                $tsfile = $result['data']['tsfile'];
+                header("Content-type: text/tab-separated-values");
+                header("Content-Length: " . filesize($tsfile));
+                header("Content-Disposition: attachment; filename=\"" . $dlfile . "\"" );
+                header("Content-Description: PHP/INTERBASE Generated Data" );
+                readfile($tsfile);
+                break;
+        }
+    }
+    
     
