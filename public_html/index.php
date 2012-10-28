e79273160accffcd6f17d1544c2689d221fa97ce
erickb <eric.kemp-benedict@sei.org>
2012-10-28 07:16:45 +0000

    Part of a fix to a bug reported by Tom: weren't showing the correct year when going from the scorecard. Now allow to pass year=... in a GET.

--- public_html/index.php
+++ public_html/index.php
@@ -12,6 +12,9 @@ if (isset($_GET['iso3'])) {
     $display_params['display_ctry']['value'] = $_GET['iso3'];
     $display_params['table_view']['value'] = 'gdrs_country_report';
 }
+if (isset($_GET['year'])) {
+    $display_params['display_yr']['value'] = $_GET['year'];
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
