35fbfb3b9f761025c8bae32384f26a7c13889567
tylerkb <tyler@tylerkb.com>
2014-02-21 08:48:01 +0000

    Edits to nav menu (including all open in new tabs); improve styling of glossary bullets and h3

--- public_html/inc/calc_branding.inc.php
+++ public_html/inc/calc_branding.inc.php
@@ -3,7 +3,7 @@
                 <div id="partners">
                     <ul>
                         <li><a id="ecoequity" href="http://www.ecoequity.org">EcoEquity</a></li>
-                        <li><a id="seius" href="http://www.sei.se">SEI-US</a></li>
+                        <li><a id="seius" href="http://www.sei-international.org">SEI-US</a></li>
                     </ul>
                 </div><!-- end #partners -->
             </div><!-- end #header -->
--- public_html/inc/calc_nav_menu_main.inc.php
+++ public_html/inc/calc_nav_menu_main.inc.php
@@ -13,11 +13,9 @@
 ?>
 <div id="nav">
     <ul class="group">
-        <li><a href="http://gdrights.org/Calculator-about">About the Calculator and Scorecard</a></li>
-        <li><a href="<?php echo $scorecard_home_url;?>">Climate Equity Pledge Scorecard</a></li>
-        <li><a href="<?php echo $gloss_url;?>">Glossary</a></li>
-        <li><a href="http://gdrights.org/about">About Greenhouse Development Rights</a></li>
-        <li><a href="http://www.ecoequity.org">EcoEquity </a></li>
-        <li class="last"><a href="http://www.sei-international.org">SEI</a></li>
+        <li><a href="http://gdrights.org/Calculator-about" target="_blank">About the Calculator and Scorecard</a></li>
+        <li><a href="<?php echo $scorecard_home_url;?>" target="_blank">Climate Equity Pledge Scorecard</a></li>
+        <li><a href="<?php echo $gloss_url;?>" target="_blank">Glossary</a></li>
+        <li class="last"><a href="http://gdrights.org/about" target="_blank">About Greenhouse Development Rights</a></li>
     </ul>
 </div><!-- end #nav -->
