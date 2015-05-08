e163a7ecd2d13b39b13217215020c76e08513dab
Christian Holz <725021+krueschan@users.noreply.github.com>
2015-05-08 03:14:27 +0000

    - slight caveat help text improvements

--- public_html/databasemanager/pledgedb/caveat_help.php
+++ public_html/databasemanager/pledgedb/caveat_help.php
@@ -20,14 +20,14 @@
             <li>It is ok to mark up the texts within the JSON values with html, but straight double quotes must be written as html entity, i.e. &amp;quot;</li> 
             <li>Currently, these keys are used by the calculator
             <ul><li>description_override = user defined pledge description</li>
-                <li>help_link = link text for help text popup for user defined pledge</li>
+                <li>help_link = link text for help text popup for user defined pledge<br />Start the help_link with a &lt;br&gt; tag to make the link appear underneath, rather than to the right of, the pledge description.</li>
                 <li>help_title = title of the help text popup</li>
-                <li>help_text = the text of the popup <br>links to other glossary items work using this syntax: &lt;a href=glossary.php#gloss_rci target=_self&gt; - note that there cannot be any quotes, single or otherwise in this html tag</li>
+                <li>help_text = the text of the popup <br>links to other glossary items work using this syntax: &lt;a href=glossary.php#gloss_rci target=_self&gt; - note that there cannot be any quotes, single or otherwise in this html tag<br />It is recommended to break help text into paragraphs using &lt;p&gt; tags, in fact their use is encouraged even for single paragraph help texts (for css)</li>
             </ul>
         </ul>
         <br /><br /> 
         Here is an example:<br />
-        <textarea name="caveat" cols="120" rows="12" class="mceNoEditor">{"description_override":"reduce total emissions by 22% compared to Mexican INDC baseline", "help_label":"<b>important information on baseline calibration</b>", "help_title":"baseline calibration", "help_text":"Mexico has provided a BAU baseline in its INDC submissions. This BAU projection differs from the <a href=glossary.php#gloss_bau target=_self>no-policies baseline</a> used by the Climate Equity Reference Calculator. We have therefore adjusted the Mexican INDC pledge to match the baseline used by the calculator. The target emissions in 2030 are identical for both methods."}
+        <textarea name="caveat" cols="120" rows="12" class="mceNoEditor">{"description_override":"reduce total emissions by 22% compared to Mexican INDC baseline", "help_label":"<br><b>important information on baseline calibration</b>", "help_title":"INDC Baseline Calibration", "help_text":"<p>Mexico has provided a BAU baseline in its INDC submissions. This BAU projection differs from the <a href=glossary.php#gloss_bau target=_self>no-policies baseline</a> used by the Climate Equity Reference Calculator. We have therefore adjusted the Mexican INDC pledge to match the baseline used by the calculator. The target emissions in 2030 are identical for both methods.</p>"}
 
 Calculation of target "hack": (1) BAU(2030) in INDC submission (GHG only, no Black Carbon) is 973 Mt CO2e); (2) 2030 non-conditional target is 22% below BAU in 2030; (3) (1) and (2) gives an emissions target of 758,940 kt CO2e; (4) absolute 2000 emissions for MEX are 618,040Kt CO2e. (5) from (3) and (4) follows that the 2030 target is a limitation to 122.8% of 2020 levels. </textarea>
     </body>
