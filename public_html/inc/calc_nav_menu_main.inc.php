441f646a6a064dd61450b2d1649273b70a095587
toma-ecoequity <toma@ecoequity.org>
2015-05-13 19:35:56 +0000

    Added Project About to Calculator Nav bar

--- public_html/inc/calc_nav_menu_main.inc.php
+++ public_html/inc/calc_nav_menu_main.inc.php
@@ -1,5 +1,6 @@
 <div id="nav">
     <ul class="group">
+        <li><a href="http://<?php echo $host_name; ?>/the-climate-equity-reference-project" target="_blank">About the Climate Equity Reference Project</a></li>
         <li><a href="http://<?php echo $host_name; ?>/calculator-about" target="_blank">About the Climate Equity Reference Calculator</a></li>
 <!--        <li><a href="<?php echo $scorecard_home_url;?>" target="_blank">Climate Equity Pledge Scorecard</a></li>-->
         <li class="last"><a href="<?php echo $gloss_url;?>" target="_blank">Glossary</a></li>
