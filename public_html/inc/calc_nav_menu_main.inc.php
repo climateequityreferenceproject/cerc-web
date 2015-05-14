b758d6f1e68fcd7d21d3b81839238b9c291729b9
toma-ecoequity <toma@ecoequity.org>
2015-05-14 04:24:09 +0000

    Reordered Nav bar to improve More Info experience

--- public_html/inc/calc_nav_menu_main.inc.php
+++ public_html/inc/calc_nav_menu_main.inc.php
@@ -3,7 +3,7 @@
         <li><a href="http://<?php echo $host_name; ?>/the-climate-equity-reference-project" target="_blank">About the Climate Equity Reference Project</a></li>
         <li><a href="http://<?php echo $host_name; ?>/calculator-about" target="_blank">About the Climate Equity Reference Calculator</a></li>
 <!--        <li><a href="<?php echo $scorecard_home_url;?>" target="_blank">Climate Equity Pledge Scorecard</a></li>-->
-        <li class="last"><a href="<?php echo $gloss_url;?>" target="_blank">Glossary</a></li>
+        <li class="last"><a href="<?php echo $host_name;?>/documentation-glossary-help/" target="_blank">Online Documentation, Glossary, Help</a></li>
 <!--        <li class="last"><a href="http://gdrights.org/about" target="_blank">About Greenhouse Development Rights</a></li>-->
     </ul>
 </div><!-- end #nav -->
