25cd960d4902b5dc0005ee8955e061e549861924
Christian Holz <cholz@climate.works>
2018-10-09 16:44:05 -0400

    Create index.php

---
+++ public_html/databasemanager/index.php
@@ -0,0 +1,10 @@
+<html>
+<head><title>CERc Database Manager</title></head>
+<body>
+The CERc Database Manager has two components:
+<ol>
+<li>The <a href=pledgedb>Pledge Database manager</a>, where countries mitigation pledges can be entered or managed to be shown for assessment purposes in the CERc country chart.</li>
+<li>The <a href=pledgedb>Help Database manager</a>, where the CERc's glossary and help text items can be managed.</li>
+</ol>
+</body>
+</html>
